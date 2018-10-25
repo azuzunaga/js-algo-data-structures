bce9af7bbb4f225aece59143e1dc242a80baa487
Americo <ame.zuz@gmail.com>
2018-10-24 20:59:39 -0700

    Use a has and object.keys and object.values to find solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const strCount = {};
+  str.split('').forEach(char => {
+    if (strCount[char]) {
+      strCount[char] += 1;
+    } else {
+      strCount[char] = 1;
+    }
+  });
+
+  const maxCount = Math.max(...Object.values(strCount));
+  const maxIdx = Object.values(strCount).indexOf(maxCount);
+
+  return Object.keys(strCount)[maxIdx];
+}
 
 module.exports = maxChar;
