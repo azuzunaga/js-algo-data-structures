d2a06f76edf9e602bc640aa3898685a4970cffb0
Americo <ame.zuz@gmail.com>
2018-12-06 22:25:30 -0800

    My naive solution

--- exercises/10-capitalize/index.js
+++ exercises/10-capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str
+    .split(' ')
+    .map(word => capitalizeWord(word))
+    .join(' ');
+}
+
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
 
 module.exports = capitalize;
