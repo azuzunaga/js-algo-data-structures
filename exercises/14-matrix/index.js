c931d5bbc3b1d3b9237fe271dee1a9a130f7d6d7
Americo <ame.zuz@gmail.com>
2018-12-14 11:51:57 -0800

    Stephens solution: walk through array in direction of spiral with for loops nested in a while loop

--- exercises/14-matrix/index.js
+++ exercises/14-matrix/index.js
@@ -16,30 +16,72 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-  const results = [...Array(n)].map(e => Array(n));
-  let [start_row, end_row] = [0, n - 1];
-  let [start_col, end_col] = [0, n - 1];
-  let [current_row, current_col] = [start_row, start_col];
-  const goal = n * n;
-  let num = 1;
-  while (num <= goal) {
-    console.log(current_row, current_col);
-    results[current_row][current_col] = num;
-    console.log(results);
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
 
-    if (current_col === end_col && current_row !== end_row) {
-      current_row++;
-    } else if (current_row === end_row && current_row !== start_row) {
-      start_row++;
-      current_col--;
-    } else if (current_row === end_row || current_row > start_row) {
-      current_row--;
-    } else if (current_col <= end_col && current_row <= end_row) {
-      current_col++;
+  let counter = 1;
+  let [startRow, endRow] = [0, n - 1];
+  let [startCol, endCol] = [0, n - 1];
+
+  while (startCol <= endCol && startRow <= endRow) {
+    // Top row
+    for (let i = startCol; i <= endCol; i++) {
+      results[startRow][i] = counter;
+      counter++;
     }
-    num++;
+    startRow++;
+
+    // Right column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+
+    // Bottom row
+    for (let i = endCol; i >= startCol; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    // Left column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startCol] = counter;
+      counter++;
+    }
+    startCol++;
   }
   return results;
 }
+
+// function matrix(n) {
+//   const results = [...Array(n)].map(e => Array(n));
+//   let [start_row, end_row] = [0, n - 1];
+//   let [start_col, end_col] = [0, n - 1];
+//   let [current_row, current_col] = [start_row, start_col];
+//   const goal = n * n;
+//   let num = 1;
+//   while (num <= goal) {
+//     console.log(current_row, current_col);
+//     results[current_row][current_col] = num;
+//     console.log(results);
+
+//     if (current_col === end_col && current_row !== end_row) {
+//       current_row++;
+//     } else if (current_row === end_row && current_row !== start_row) {
+//       start_row++;
+//       current_col--;
+//     } else if (current_row === end_row || current_row > start_row) {
+//       current_row--;
+//     } else if (current_col <= end_col && current_row <= end_row) {
+//       current_col++;
+//     }
+//     num++;
+//   }
+//   return results;
+// }
 
 module.exports = matrix;
