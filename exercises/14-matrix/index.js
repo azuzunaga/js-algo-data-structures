121a8729d83e8f30e92afa75a450d24657807a30
Americo <ame.zuz@gmail.com>
2018-12-14 11:36:43 -0800

    My incomplete solution

--- exercises/14-matrix/index.js
+++ exercises/14-matrix/index.js
@@ -15,6 +15,31 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [...Array(n)].map(e => Array(n));
+  let [start_row, end_row] = [0, n - 1];
+  let [start_col, end_col] = [0, n - 1];
+  let [current_row, current_col] = [start_row, start_col];
+  const goal = n * n;
+  let num = 1;
+  while (num <= goal) {
+    console.log(current_row, current_col);
+    results[current_row][current_col] = num;
+    console.log(results);
+
+    if (current_col === end_col && current_row !== end_row) {
+      current_row++;
+    } else if (current_row === end_row && current_row !== start_row) {
+      start_row++;
+      current_col--;
+    } else if (current_row === end_row || current_row > start_row) {
+      current_row--;
+    } else if (current_col <= end_col && current_row <= end_row) {
+      current_col++;
+    }
+    num++;
+  }
+  return results;
+}
 
 module.exports = matrix;
